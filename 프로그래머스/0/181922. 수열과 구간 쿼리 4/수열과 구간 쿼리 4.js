89c4e61f36fb3e0b90b600a5e6255841df7c50ed
jiyeon <94913420+jiyeonyun@users.noreply.github.com>
2024-01-30 22:53:27 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 4.47 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -0,0 +1,16 @@
+function solution(arr, queries) {
+    let answer = [...arr];
+    for(let i =0 ; i<queries.length; i++){
+        let s = queries[i][0];
+        let e = queries[i][1];
+        let k = queries[i][2];
+    
+        for (let j = s; j <= e; j++) {
+            if (j % k === 0) {
+                answer[j]++;
+            }
+        }
+       
+    }
+    return answer;
+}
