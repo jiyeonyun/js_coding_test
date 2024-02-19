5d4c4a4c7813eacb6fd9b8c925ae636fae7d29a4
jiyeon <94913420+jiyeonyun@users.noreply.github.com>
2024-02-19 12:22:23 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -1,14 +1,10 @@
 function solution(binomial) {
     var answer = 0;
-    let op = "";
-    for (let i = 0; i < binomial.length; i++) {
-        const item = binomial[i];
-        isNaN(item) && (op = item);
-    }
-    let binomialArr = binomial.split(op);
-
+    let binomialArr = binomial.split(" ");
+    
     const num1 = parseFloat(binomialArr[0]);
-    const num2 = parseFloat(binomialArr[1]);
+    const op = binomialArr[1];
+    const num2 = parseFloat(binomialArr[2]);
 
     switch (op) {
         case "+":
