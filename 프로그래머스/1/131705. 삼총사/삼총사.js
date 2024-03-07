0038b4823e63ad78807c1d6d28d663d2be9c3195
jiyeon <94913420+jiyeonyun@users.noreply.github.com>
2024-03-07 12:32:20 +0900

    [level 1] Title: 삼총사, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,17 @@
+function solution(number) {
+    let count = 0;
+    const n = number.length;
+
+    for (let i = 0; i < n - 2; i++) {
+        for (let j = i + 1; j < n - 1; j++) {
+            for (let k = j + 1; k < n; k++) {
+                
+                if (number[i] + number[j] + number[k] === 0) {
+                    count++;
+                }
+            }
+        }
+    }
+    
+    return count;
+}
