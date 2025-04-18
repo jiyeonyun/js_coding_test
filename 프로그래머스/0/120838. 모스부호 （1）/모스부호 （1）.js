240238cc93bf69a40813f51dd5611b611dad9378
jiyeon <94913420+jiyeonyun@users.noreply.github.com>
2025-04-18 16:17:08 +0900

    [level 0] Title: 모스부호 (1), Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,16 @@
+function solution(letter) {
+    const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+    const morseLetter = letter.split(" ");
+   const newArr =  morseLetter.map((item)=>{
+        return morse[item]
+    })
+ 
+    var answer = newArr.join("");
+    return answer;
+}
