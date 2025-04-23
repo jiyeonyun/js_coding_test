2670e4aec14dcdc559ca37e41bc146a826e47865
jiyeon <94913420+jiyeonyun@users.noreply.github.com>
2025-04-23 15:39:34 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,16 @@
+function solution(my_string) {
+    const arr = my_string.split(" ");
+    let answer = Number(arr[0]);
+
+for (let i = 1; i < arr.length; i += 2) {
+    const op = arr[i];
+    const num = Number(arr[i + 1]);
+
+    if (op === "+") {
+       answer = answer + num
+    } else {
+        answer = answer - num
+    }
+}
+    return answer
+}
