3fc29a02fb4f223fce41f77f9b9ddade55c19fc7
jiyeon <94913420+jiyeonyun@users.noreply.github.com>
2025-09-19 12:58:48 +0900

    [level 1] Title: 실패율, Time: 1635.16 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,21 +1,21 @@
 function solution(N, stages) {
-    var answer = new Array(N).fill(0);
-    for(let i = 1; i<=N; i++){
-        let user = 0 
+    var answer = new Array(N).fill(0)
+
+    for(let i=1; i<=N; i++){
+        let user = 0
         let dojun = 0
         stages.forEach((item)=>{
-            if(item==i){
+            if(item == i){
                 dojun++
             }
-            if (item>=i){
+            if(item >= i){
                 user++
             }
-            
-            
         })
         answer[i-1] = {score:dojun/user,stage:i}
+    
     }
-   answer =  answer.sort((a,b)=>{
+      answer =  answer.sort((a,b)=>{
        if(a.score < b.score) return 1;
       if(a.score > b.score) return -1;
       if(a.score === b.score) return 0;
