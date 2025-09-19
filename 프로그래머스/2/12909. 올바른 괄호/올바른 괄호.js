e601a17a3c1453ff21b033039039009ec0aecb13
jiyeon <94913420+jiyeonyun@users.noreply.github.com>
2025-09-19 13:31:25 +0900

    [level 2] Title: 올바른 괄호, Time: 31.61 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,12 +1,15 @@
-function solution(s){
-    var answer = true;
-    let sArr =[...s]
-    let stack = []
-
-   sArr.forEach((item)=>{
-      item == "(" ? stack.push(item): stack.length>0?stack.pop():stack.push(item)
-    })
-
-    stack.length == 0 ?answer = true : answer = false
-    return answer;
+function solution(s) {
+    let stack = [];
+    for (let st of s) {
+        if (st === "(") {
+            stack.push(st);
+        } else {
+            if (stack.length === 0) {
+                return false;
+            }
+            stack.pop();
+        }
+    }
+  
+    return stack.length === 0;
 }
